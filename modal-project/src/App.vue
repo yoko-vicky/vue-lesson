b1d0343fc5bbdfdb8a3a7ea3641558bd837bc0a9
yocosaka <sakayoco55@gmail.com>
2021-09-03 22:56:03 +0900

    Add modal-project finished until Template Refs section

---
+++ modal-project/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="booo">
+  <button @click="handleClick">Click Me</button>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return {
+      title: 'My first view app :)'
+    }
+  },
+  methods: {
+    handleClick(){
+      console.log(this.$refs.booo.value)
+      console.log(this.$refs.booo.classList)
+      this.$refs.booo.classList.add('active')
+      this.$refs.booo.focus()
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
