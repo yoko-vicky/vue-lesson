d4a36c089d69c4c5d328134b1b1882abaefbc494
yocosaka <sakayoco55@gmail.com>
2021-09-10 19:11:53 +0900

    Add programmatic Navigation such as goback, goforward and redirection

--- my-jobs/src/App.vue
+++ my-jobs/src/App.vue
@@ -4,8 +4,32 @@
     <router-link :to="{ name: 'About'}">About</router-link> |
     <router-link :to="{ name: 'Jobs'}">Jobs</router-link>
   </div>
+
+  <!-- Programmatic Navigation -->
+  <button @click="redirect">Redirect</button>
+  <button @click="goBack">Go back</button>
+  <button @click="goForward">Go forward</button>
+
   <router-view/>
 </template>
+
+<script>
+export default {
+  methods: {
+    redirect(){
+      this.$router.push({ name: 'Home'})
+    },
+    goBack(){
+      this.$router.go(-1)
+    },
+    goForward(){
+      this.$router.go(1)
+    }
+  }
+}
+</script>
+
+
 
 <style>
 #app {
@@ -32,5 +56,13 @@
   /* color: #42b983; */
   color: white;
   background: crimson;
+}
+
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
 }
 </style>
