46411f09bf184f5708c3a7e6564ca04656115f2e
yocosaka <sakayoco55@gmail.com>
2021-09-14 13:33:57 +0900

    Add watch and watchEffect

--- vue-blog/src/views/Home.vue
+++ vue-blog/src/views/Home.vue
@@ -4,11 +4,13 @@
     <input type="text" v-model="search">
     <p>Search term: {{ search }}</p>
     <div v-for="name in macthingNames" key="name">{{ name }}</div>
+    <button @click="handleStopWatch">Stop Watching</button>
+    <button @click="handleStopWatchEffect">Stop Watch Effect</button>
   </div>
 </template>
 
 <script>
-import { computed, ref } from 'vue'
+import { computed, ref, watch, watchEffect } from 'vue'
 
 export default {
   name: 'Home',
@@ -16,11 +18,29 @@ export default {
     const search = ref('')
     const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
 
+    const stopWatchForSearch = watch(search, ()=>{
+      // Everytime search changes, do something
+      console.log('watch running')
+    })
+
+    const stopWatchEffect = watchEffect(()=>{
+      // Eny updates/changes on variables inside this useEffect function will be detected
+      console.log('watchEffect Function running', search.value)
+    })
+
     const macthingNames = computed(()=>{
       return names.value.filter((name)=> name.includes(search.value))
     })
 
-    return { names, search, macthingNames }
+    const handleStopWatch = () => {
+      stopWatchForSearch()
+    }
+
+    const handleStopWatchEffect = () => {
+      stopWatchEffect()
+    }
+
+    return { names, search, macthingNames, handleStopWatch, handleStopWatchEffect }
   }
 }
 </script>
